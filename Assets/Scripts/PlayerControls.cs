07250916a4045b810c970f31e3b6e91c26543f97
Maximiliam Karlsson <admin@example.com>
2019-04-15 16:38:50 +0200

    Player Controls added

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+        rb.AddForce(movement * speed);
+    }
+}

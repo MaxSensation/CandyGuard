45fdf4f9ce494a50cfdeebefd4d820efffaa85c8
Linevs22 <42997155+Linevs22@users.noreply.github.com>
2019-04-29 16:34:16 +0200

    Added candy animation test

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public Animation candyAnimation;
+
+
+    private void Start()
+    {
+        candyAnimation.wrapMode = WrapMode.Once;
+        candyAnimation.Play();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}

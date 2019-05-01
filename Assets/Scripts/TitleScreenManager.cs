9b80390b9001a59daa563685855ce9e8c39cee51
Linevs22 <42997155+Linevs22@users.noreply.github.com>
2019-05-01 16:10:42 +0200

    Tried fixing animation
    It's better but still does not work as intended

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class TitleScreenManager : MonoBehaviour
 {
     public Animator candyAnimator;
+    public GameObject startScreen;
 
 
     private void Start()
     {
-
+        candyAnimator = GetComponent<Animator>();
+        startScreen = GameObject.Find("Start Screen");
     }
 
 
     void Update()
     {
-        
+        if (startScreen.activeInHierarchy) 
+        {
+            candyAnimator.Play("Candy Animation", 0, 0);
+        }
     }
 }

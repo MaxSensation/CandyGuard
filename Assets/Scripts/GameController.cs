fc19d6a4ea6240e891dd286e3321873ef20a633e
KarlssonMaximiliam <maka4519@student.su.se>
2019-05-07 19:07:59 +0200

    Just rearranged some code and added icon

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -70,6 +70,232 @@ public class GameController : MonoBehaviour
         SetCandyBagColors();
     }
 
+    public int GetCandyTypesGroup()
+    {
+        return candyTypesGroup;
+    }
+
+    public float GetCandySpawnTime()
+    {
+        return candySpawnTime;
+    }
+
+    public float GetBonusCandySpawnTime()
+    {
+        return bonusCandySpawnTime;
+    }    
+
+    public float GetCandySpeed()
+    {
+        return candySpeed;
+    }
+
+
+    private void SetCandyBagColors()
+    {
+        candyBags[0].GetComponentInChildren<SpriteRenderer>().color = bottomColors[0];
+        candyBags[2].GetComponentInChildren<SpriteRenderer>().color = bottomColors[1];
+        candyBags[4].GetComponentInChildren<SpriteRenderer>().color = bottomColors[2];
+        candyBags[6].GetComponentInChildren<SpriteRenderer>().color = bottomColors[3];
+        candyBags[1].GetComponentInChildren<SpriteRenderer>().color = topColors[0];
+        candyBags[3].GetComponentInChildren<SpriteRenderer>().color = topColors[1];
+        candyBags[5].GetComponentInChildren<SpriteRenderer>().color = topColors[2];
+        candyBags[7].GetComponentInChildren<SpriteRenderer>().color = topColors[3];
+    }
+
+    public Color32 GetTopColor(int lane)
+    {
+        return topColors[lane - 1];
+    }
+
+    public void AddActiveCandy()
+    {
+        candiesActive += 1;
+    }
+
+    public void RemoveActiveCandy()
+    {
+        candiesActive -= 1;
+    }
+
+    public Color32 GetBottomColor(int lane)
+    {
+        return bottomColors[lane - 1];
+    }
+
+    public void AddScore(int points)
+    {
+        currentScore += points;
+        scoreText.text = currentScore.ToString();
+        if (currentScore >= targetScore)
+        {
+            SetDifficultyChangeActive();
+        }
+    }
+
+    void Update()
+    {
+        if (difficultyChangeActive && candiesActive == 0)
+        {            
+            IncreaseDifficulty();
+            levelText.text = difficulty.ToString();
+        }
+    }
+
+    private void SetDifficultyChangeActive()
+    {
+        difficultyChangeActive = true;
+    }
+
+    private void IncreaseDifficulty()
+    {        
+        difficulty += 1;
+        SetDifficulty(difficulty);
+        SetCandyBagColors();
+        difficultyChangeActive = false;
+    }
+
+    public void GameOver()
+    {
+        gameover = true;
+        gameoverUI.SetActive(true);
+        if (currentScore > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", currentScore);
+            highScoreUI.transform.Find("ScoreText").GetComponent<Text>().text = currentScore.ToString();
+            highScoreUI.SetActive(true);            
+        }
+    }
+
+    private void SetGoals(int goalLevel)
+    {
+        Color32 green = new Color32(130, 255, 130, 255);
+        Color32 red = new Color32(255, 100, 100, 255);
+        Color32 orange = new Color32(255, 175, 90, 255);
+        Color32 yellow = new Color32(255, 250, 110, 255);
+
+        if (goalLevel == 1)
+        {
+            topColors = new Color32[4] {
+                red,
+                red,
+                red,
+                red
+            };
+            bottomColors = new Color32[4] {
+                green,
+                green,
+                green,
+                green
+            };
+        }
+        else if (goalLevel == 2)
+        {
+            topColors = new Color32[4] {
+                green,
+                green,
+                green,
+                green
+            };
+            bottomColors = new Color32[4] {
+                red,
+                red,
+                red,
+                red
+            };
+        }
+        else if (goalLevel == 3)
+        {
+            topColors = new Color32[4] {
+                green,
+                red,
+                green,
+                red
+            };
+            bottomColors = new Color32[4] {
+                red,
+                green,
+                red,
+                green
+            };
+        }
+        else if (goalLevel == 4)
+        {
+            topColors = new Color32[4] {
+                red,
+                green,
+                red,
+                green
+            };
+            bottomColors = new Color32[4] {
+                green,
+                red,
+                green,
+                red
+            };
+        }
+        else if (goalLevel == 5)
+        {
+            topColors = new Color32[4] {
+                green,
+                red,
+                green,
+                yellow
+            };
+            bottomColors = new Color32[4] {
+                yellow,
+                green,
+                red,
+                green
+            };
+        }
+        else if (goalLevel == 6)
+        {
+            topColors = new Color32[4] {
+                red,
+                green,
+                yellow,
+                green
+            };
+            bottomColors = new Color32[4] {
+                green,
+                red,
+                green,
+                yellow
+            };
+        }
+        else if (goalLevel == 7)
+        {
+            topColors = new Color32[4] {
+                green,
+                yellow,
+                red,
+                yellow
+            };
+            bottomColors = new Color32[4] {
+                yellow,
+                red,
+                yellow,
+                green
+            };
+        }
+        else if (goalLevel == 7)
+        {
+            topColors = new Color32[4] {
+                yellow,
+                red,
+                yellow,
+                green
+            };
+            bottomColors = new Color32[4] {
+                red,
+                yellow,
+                green,
+                yellow
+            };
+        }
+    }
+
     private void SetDifficulty(int difficulty)
     {
         if (difficulty == 1)
@@ -167,232 +393,6 @@ public class GameController : MonoBehaviour
             candyTypesGroup = 1;
             candySpawnTime = 1.5f;
             candySpeed = 3.2f;
-        }
-    }
-
-    public int GetCandyTypesGroup()
-    {
-        return candyTypesGroup;
-    }
-
-    public float GetCandySpawnTime()
-    {
-        return candySpawnTime;
-    }
-
-    public float GetBonusCandySpawnTime()
-    {
-        return bonusCandySpawnTime;
-    }    
-
-    public float GetCandySpeed()
-    {
-        return candySpeed;
-    }
-
-
-    private void SetCandyBagColors()
-    {
-        candyBags[0].GetComponentInChildren<SpriteRenderer>().color = bottomColors[0];
-        candyBags[2].GetComponentInChildren<SpriteRenderer>().color = bottomColors[1];
-        candyBags[4].GetComponentInChildren<SpriteRenderer>().color = bottomColors[2];
-        candyBags[6].GetComponentInChildren<SpriteRenderer>().color = bottomColors[3];
-        candyBags[1].GetComponentInChildren<SpriteRenderer>().color = topColors[0];
-        candyBags[3].GetComponentInChildren<SpriteRenderer>().color = topColors[1];
-        candyBags[5].GetComponentInChildren<SpriteRenderer>().color = topColors[2];
-        candyBags[7].GetComponentInChildren<SpriteRenderer>().color = topColors[3];
-    }
-
-    public Color32 GetTopColor(int lane)
-    {
-        return topColors[lane - 1];
-    }
-
-    public void AddActiveCandy()
-    {
-        candiesActive += 1;
-    }
-
-    public void RemoveActiveCandy()
-    {
-        candiesActive -= 1;
-    }
-
-    public Color32 GetBottomColor(int lane)
-    {
-        return bottomColors[lane - 1];
-    }
-
-    private void SetGoals(int goalLevel)
-    {
-        Color32 green = new Color32(130, 255, 130, 255);
-        Color32 red = new Color32(255, 100, 100, 255);
-        Color32 orange = new Color32(255, 175, 90, 255);
-        Color32 yellow = new Color32(255, 250, 110, 255);
-
-        if (goalLevel == 1)
-        {
-            topColors = new Color32[4] {
-                red,
-                red,
-                red,
-                red
-            };
-            bottomColors = new Color32[4] {
-                green,
-                green,
-                green,
-                green
-            };
-        }
-        else if (goalLevel == 2)
-        {
-            topColors = new Color32[4] {
-                green,
-                green,
-                green,
-                green
-            };
-            bottomColors = new Color32[4] {
-                red,
-                red,
-                red,
-                red
-            };            
-        }
-        else if (goalLevel == 3)
-        {
-            topColors = new Color32[4] {
-                green,
-                red,
-                green,
-                red
-            };
-            bottomColors = new Color32[4] {
-                red,
-                green,
-                red,
-                green                
-            };
-        }
-        else if (goalLevel == 4)
-        {
-            topColors = new Color32[4] {                
-                red,
-                green,
-                red,
-                green   
-            };
-            bottomColors = new Color32[4] {                
-                green,
-                red,
-                green,
-                red
-            };
-        }
-        else if (goalLevel == 5)
-        {
-            topColors = new Color32[4] {
-                green,
-                red,
-                green,
-                yellow
-            };
-            bottomColors = new Color32[4] {
-                yellow,
-                green,
-                red,
-                green
-            };
-        }
-        else if (goalLevel == 6)
-        {
-            topColors = new Color32[4] {                
-                red,
-                green,
-                yellow,
-                green
-            };
-            bottomColors = new Color32[4] {                
-                green,
-                red,
-                green,
-                yellow
-            };
-        }
-        else if (goalLevel == 7)
-        {
-            topColors = new Color32[4] {
-                green,
-                yellow,
-                red,
-                yellow
-            };
-            bottomColors = new Color32[4] {
-                yellow,
-                red,
-                yellow,
-                green
-            };
-        }
-        else if (goalLevel == 7)
-        {
-            topColors = new Color32[4] {                
-                yellow,
-                red,
-                yellow,
-                green
-            };
-            bottomColors = new Color32[4] {                
-                red,
-                yellow,
-                green,
-                yellow
-            };
-        }
-    }
-
-    public void AddScore(int points)
-    {
-        currentScore += points;
-        scoreText.text = currentScore.ToString();
-        if (currentScore >= targetScore)
-        {
-            SetDifficultyChangeActive();
-        }
-    }
-
-    void Update()
-    {
-        if (difficultyChangeActive && candiesActive == 0)
-        {            
-            IncreaseDifficulty();
-            levelText.text = difficulty.ToString();
-        }
-    }
-
-    private void SetDifficultyChangeActive()
-    {
-        difficultyChangeActive = true;
-    }
-
-    private void IncreaseDifficulty()
-    {        
-        difficulty += 1;
-        SetDifficulty(difficulty);
-        SetCandyBagColors();
-        difficultyChangeActive = false;
-    }
-
-    public void GameOver()
-    {
-        gameover = true;
-        gameoverUI.SetActive(true);
-        if (currentScore > PlayerPrefs.GetInt("HighScore", 0))
-        {
-            PlayerPrefs.SetInt("HighScore", currentScore);
-            highScoreUI.transform.Find("ScoreText").GetComponent<Text>().text = currentScore.ToString();
-            highScoreUI.SetActive(true);            
         }
     }
 }

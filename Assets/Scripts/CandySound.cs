d2359958ff39d0eb0a6387a15318f28217417461
KarlssonMaximiliam <maka4519@student.su.se>
2019-05-07 17:23:13 +0200

    Fixed sound

---
+++ Assets/Scripts/CandySound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySound : MonoBehaviour
+{
+    public AudioClip[] bouncSounds;
+    private AudioSource audioSouce;
+
+    private void OnTriggerEnter2D(Collider2D colider)
+    {
+        if (colider.gameObject.tag == "Player")
+        {
+            audioSouce = GetComponent<AudioSource>();
+            audioSouce.clip = bouncSounds[Random.Range(0, bouncSounds.Length)];
+            audioSouce.Play();
+        }
+    }
+}

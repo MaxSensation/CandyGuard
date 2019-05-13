9fe04bb54b7be31cdbef71ff2d1eeae4deb64e86
Linevs22 <42997155+Linevs22@users.noreply.github.com>
2019-05-13 14:03:16 +0200

    Scrips for volume sliders added
    audio source needs to be attached

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public Slider MusicVolume;
+    public Slider EffectVolume;
+
+    public AudioSource Music;
+    public AudioSource Effect;
+
+    void Update()
+    {
+        Music.volume = MusicVolume.value;
+        Effect.volume = EffectVolume.value;
+    }
+}

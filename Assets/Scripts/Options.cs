0395da87fa7938b6063e9baf2f634b740a65f96b
KarlssonMaximiliam <maka4519@student.su.se>
2019-05-21 14:01:46 +0200

    Added function for colorblindmode

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -9,10 +9,19 @@ public class Options : MonoBehaviour
     public Slider effectVolume;
     public static Options instance = null;
     private AudioSource menuMusic;
+    private bool colorBlindMode = false;
 
     private void Start()
     {
         menuMusic = GetComponentInParent<AudioSource>();
+
+        if (PlayerPrefs.GetInt("ColorBlind", 0) == 0)
+            colorBlindMode = false;
+        else
+        {
+            colorBlindMode = true;
+            GameObject.Find("ColorblindmodeToggle").GetComponent<Toggle>().isOn = true;
+        }            
     }
 
     public void Update()
@@ -36,5 +45,23 @@ public class Options : MonoBehaviour
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
+    }
+
+    public bool IsColorBlindMode()
+    {
+        return colorBlindMode;
+    }
+
+    public void SetColorBlindMode(bool toggle)
+    {
+        colorBlindMode = toggle;
+        if (toggle == true)
+        {
+            PlayerPrefs.SetInt("ColorBlind", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("ColorBlind", 0);
+        }
     }
 }

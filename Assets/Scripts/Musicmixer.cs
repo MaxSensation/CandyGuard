eab5bcf05c7d9e6519ef7f7b4816816be5dd0e89
Kezze16 <kez.fds@gmail.com>
2019-05-22 15:14:01 +0200

    Added 2 music
    Gameplay music added with a script

---
+++ Assets/Scripts/Musicmixer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musicmixer : MonoBehaviour
+{
+    private AudioSource aud;
+    public AudioClip clip1;
+    public AudioClip clip2;
+     
+   
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+       
+        if (PlayerPrefs.GetString("GameMode", "") == "Endless")
+        {
+            aud.clip = clip1; 
+        }
+        if (PlayerPrefs.GetString("GameMode", "") == "Timed") {
+
+            aud.clip = clip2; 
+
+        }
+
+        aud.Play();
+    }
+
+   
+}

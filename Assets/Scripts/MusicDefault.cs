d07e71264688013669bcbda92fadf2a0817a7fc9
KarlssonMaximiliam <maka4519@student.su.se>
2019-05-22 20:41:37 +0200

    SoundFixed

---
+++ Assets/Scripts/MusicDefault.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicDefault : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Slider>().value = PlayerPrefs.GetFloat("MusicVolume", 1);
+        GetComponent<Slider>().onValueChanged.AddListener(UpdateValue);
+    }
+
+    private void UpdateValue(float value)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", value);
+        MusicMixer.instance.SetVolume(value);
+    }
+}

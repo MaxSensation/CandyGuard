4985d99e6c54d801915953ab3e64991df2526f0a
KarlssonMaximiliam <maka4519@student.su.se>
2019-05-24 14:48:02 +0200

    Fixed toggle support

--- Assets/Scripts/PlayButtonSound.cs
+++ Assets/Scripts/PlayButtonSound.cs
@@ -7,12 +7,23 @@ public class PlayButtonSound : MonoBehaviour
 {    
     private void Start()
     {
-        GetComponent<Button>().onClick.AddListener(PlaySound);        
-    }   
+        if (GetComponent<Button>() == true)
+        {
+            GetComponent<Button>().onClick.AddListener(PlaySound);
+        }
+        else
+        {
+            GetComponent<Toggle>().onValueChanged.AddListener(PlaySound);
+        }
+    }
 
     private void PlaySound()
     {
         EffectMixer.instance.PlayMenuButtonSound();
     }
-}
 
+    private void PlaySound(bool toggle)
+    {
+        EffectMixer.instance.PlayMenuButtonSound();
+    }
+}

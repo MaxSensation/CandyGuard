2f69adea1c56f3387257f2ea4522655a275e2b01
KarlssonMaximiliam <maka4519@student.su.se>
2019-06-02 20:57:06 +0200

    Refaktor old code and remade the whole backgroundscroller

---
+++ Assets/Scripts/BackgroundScrollerV2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BackgroundScrollerV2 : MonoBehaviour
+{
+    public float scroll_Speed;
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        float y = Time.time * scroll_Speed * -1;
+        Vector2 offset = new Vector2(0, y);
+        meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}

efd3142816fa39dcbaf06f554e6a3026823b32e0
KarlssonMaximiliam <maka4519@student.su.se>
2019-06-03 13:55:01 +0200

    Fixed the spawnrate

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -39,7 +39,7 @@ public class LevelGenerator : MonoBehaviour
             targetScoreDefault = 50;
             candySpawnTimeDefault = 3f;
             candySpeedDefault = 2f;
-            bonusCandySpawnTimeDefault = 20f;
+            bonusCandySpawnTimeDefault = 20.5f;
         }
         else if (PlayerPrefs.GetString("GameMode", "") == "Timed" &&  lastLevel == null)        
         {
